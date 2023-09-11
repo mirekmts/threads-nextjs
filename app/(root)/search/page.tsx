f2b302c8ee77f7f3d682df19ade58720f76b2317
miroslaw.stasiak <mstasiak@onairent.live>
2023-09-11 22:53:28 +0200

    feat: add pagination and search to search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,16 @@
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
-
+import { SearchBar } from "@/components/shared/SearchBar";
+import { Pagination } from "@/components/shared/Pagination";
 import { UserCard } from "@/components/cards/UserCard";
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
-async function Page() {
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
@@ -14,14 +19,16 @@ async function Page() {
 
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      <SearchBar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
@@ -41,6 +48,12 @@ async function Page() {
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
 }
